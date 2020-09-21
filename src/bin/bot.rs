32b3554926ccd4c5f1e95424d64b187dd9c92fa1
wjzz <wjedynak@gmail.com>
2020-09-21 22:22:58 +0900

    Update UCI protocol handling

--- src/bin/bot.rs
+++ src/bin/bot.rs
@@ -13,15 +13,17 @@ fn play_move(fen: &str, moves: &[&str], wtime: i32, btime: i32) -> String {
     for mv_str in moves.iter() {
         let mv = Move::from_uci_ascii(mv_str);
         let mv = intmove_from_move(&mv);
+        eprint!("{} ", intmove_to_uci_ascii(mv));
         pos.make_move(mv).unwrap();
     }
+    eprintln!();
 
     let time_left = if pos.to_move == Player::White { wtime } else { btime };
     let final_fen = pos.to_fen();
 
     eprintln!("FEN: {}", final_fen);
     let mv = if time_left > 60 * 1000 {
-        let (mv, val) = best_move_pvs(&mut pos, 4);
+        let (mv, val) = best_move_pvs(&mut pos, 5);
         // let mv = best_move_negamax(&mut pos, 3);
         // let mv = choose_move(&pos);
         eprintln!("MOVE: {} EVAL: {:.1}", intmove_to_uci_ascii(mv), val);
@@ -52,6 +54,10 @@ fn main() {
     loop {
         let line = read_line();
 
+        if line != "" {
+            eprintln!("<< {}", line);
+        }
+
         if line == "isready" {
             println!("readyok");
         } else if line == "quit" {
@@ -59,30 +65,47 @@ fn main() {
         } else if line == "stop" {
             break;
         } else if line.starts_with("position") {
+            let fen;// = String::new();
+            let mut moves: &[&str] = &[];
+
             let parts: Vec<_> = line.split_ascii_whitespace().collect();
-            // println!("<< parts1 {:?}", parts);
-            if parts[0] == "position" {
-                let fen = parts[1];
-                let moves = if parts.len() > 2 {
+            if line.starts_with("position fen") {
+                let fen_parts = &parts[2..8];
+                fen = fen_parts.join(" ");
+                if parts.len() > 8 {
+                    assert_eq!(parts[8], "moves");
+                    moves = &parts[9..];
+                }
+            } else {
+                assert_eq!(parts[0], "position");
+                assert_eq!(parts[1], "startpos");
+
+                fen = String::from("startpos");
+
+                if parts.len() > 2 {
                     let moves_str = parts[2];
                     assert_eq!(moves_str, "moves");
-                    &parts[3..]
-                } else {
-                    &[]
-                };
+                    moves = &parts[3..]
+                }
+            }
 
-                let line2 = read_line();
-                let parts2: Vec<_> = line2.split_ascii_whitespace().collect();
-                // println!("<< parts2 {:?}", parts2);
-                // go wtime 19870 btime 27130 winc 0 binc 0
-                assert_eq!(parts2[0], "go");
-                assert_eq!(parts2[1], "wtime");
+            eprintln!("FEN IS = {}", fen);
+
+            let line2 = read_line();
+            let parts2: Vec<_> = line2.split_ascii_whitespace().collect();
+            assert_eq!(parts2[0], "go");
+            let (wtime, btime) = if parts2[1] == "wtime" {
                 let wtime: i32 = parts2[2].parse().unwrap();
                 assert_eq!(parts2[3], "btime");
                 let btime: i32 = parts2[4].parse().unwrap();
-                let bot_move = play_move(fen, moves, wtime, btime);
-                println!("bestmove {}", bot_move);
-            }
+                (wtime, btime)
+            } else {
+                assert_eq!(parts2[1], "movetime");
+                let time: i32 = parts2[2].parse().unwrap();
+                (time, time)
+            };
+            let bot_move = play_move(&fen, moves, wtime, btime);
+            println!("bestmove {}", bot_move);
         } else {
             if line != "" {
                 eprintln!("Unknown command: {}", line);
